9555295d8537daaf37703203214863d7968c1673
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-06 22:28:52 +0530

    bug: NoMethodError
    Commented line 12 for now
    error: NoMethodError in SessionsController#create undefined method `errors' for nil:NilClass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
   def create
     user=User.find_by(email:login_params[:email])
     if user && user.authenticate(login_params[:password])
       session[:user_id]=user.id
       redirect_to '/patient_dashboard/index'
     else
-      flash[:login_errors]=user.errors.full_messages
+      #flash[:login_errors]=user.errors.full_messages
       redirect_to '/users/index'
     end
   end
