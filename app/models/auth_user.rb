e5b550915884c3bf425c9e60b73956c2b60b2fd2
Punam Kumavat <punamkumavat01@gmail.com>
2023-06-19 00:51:35 +0530

    login: devise auth added
    basic gem and migration

---
+++ app/models/auth_user.rb
@@ -0,0 +1,10 @@
+class AuthUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :validatable,
+          :trackable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :auth_users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "patient_dashboard#index"
   get 'users/index'
   get '/users/login'
   post '/sessions' => 'sessions#create'
